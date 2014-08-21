93aad9455916186723b2693d974b62985d5cae1e
Plotnikov Lev <lev.plotnikov@gmail.com>
2014-08-21 12:55:07 -0700

    one more aibi ridle (august)

---
+++ ibm_ridle/august_2014.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+import sys
+
+
+avr = [0.0] *151
+tmp = [0.0] * 151
+point = [0] * 151
+
+def calc(k,n):
+    return ((10.0 + avr[n-1]) * (n-1) + (1+avr[k-n+1] ) * (k - n + 1)) / k 
+
+def calc2(k):
+    for n in range(2,k+1):
+	t = calc(k,n)
+	tmp[n] = t
+
+    # print tmp[1:k+1]
+    avr[k] = min(tmp[2:k+1])
+    point[k] = tmp[2:k+1].index(avr[k]) +2 
+
+def doit():
+  print "start"
+
+  avr[0] = 0
+  avr[1] = 0
+
+  for i in range(2,151):
+      calc2(i)
+
+  print avr
+  print point
+
+if __name__ == '__main__':
+    doit()
