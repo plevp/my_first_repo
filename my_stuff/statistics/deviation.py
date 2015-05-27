c7ec99250362a392f57b8e593cdc7b2d4a45ae23
Plotnikov Lev <lev.plotnikov@gmail.com>
2015-05-27 01:26:07 -0700

    sudoku and deviation

--- my_stuff/statistics/deviation.py
+++ my_stuff/statistics/deviation.py
@@ -7,15 +7,39 @@ def arith_mean(l):
     
 def diviation(l):
     m = arith_mean(l)
-    print m
     v = reduce(lambda a, x : a + (x -m)**2, l, 0.0)
     v = v / (len(l) -1)
     return (v, sqrt(v))
 
+def moment(l, k):
+    m = arith_mean(l)
+    v = reduce(lambda a, x : a + (x -m)**k, l, 0.0)
+    return v/len(l)
+
+def skewness(l):
+    print arith_mean(l)
+    m3 = moment(l,3)
+    m2 = moment(l,2)
+    print m2, m3
+    
+    return m3 / (m2 * sqrt(m2))
+
+def kurtosis(l):
+    m4 = moment(l, 4)
+    m2 = moment(l, 2)
+    return m4 / (m2 **2)
+
+
+
 #print diviation([1,2,3,4,5])
 #print diviation([4.9, 4.8, 6.8])
 
-print diviation([1.46, 4.16, 0.27, 9.95, 0.67, -0.14])
 
+#print skewness([ 1006.84, 15732.31, 41824.32, 415.42, 84748.37, 53041.98])
+
+# Statistics quize week 3
+print diviation([1.46, 4.16, 0.27, 9.95, 0.67, -0.14])
+print skewness([1.46, 4.16, 0.27, 9.95, 0.67, -0.14])
+print kurtosis([1.46, 4.16, 0.27, 9.95, 0.67, -0.14]) -3
 
 
--- my_stuff/sudoku/sudoku.py
+++ my_stuff/sudoku/sudoku.py
@@ -29,6 +29,7 @@ unitlist = ([cross(rows, c) for c in cols] +
             [cross(r, cols) for r in rows] +
             [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
 
+print "len(unitlist)", len(unitlist)
 #print ((s, [u for u in unitlist if s in u]) for s in squares)
 #print ((s) for s in squares)
 
@@ -52,7 +53,7 @@ def parse_grid(grid):
     return False if a contradiction is detected."""
     ## To start, every square can be any digit; then assign values from the grid.
     values = dict((s, digits) for s in squares)
-    print values['A1']
+    #print values['A1']
     for s,d in grid_values(grid).items():
         if d in digits and not assign(values, s, d):
             return False ## (Fail if we can't assign d to square s.)
@@ -64,10 +65,84 @@ def grid_values(grid):
     assert len(chars) == 81
     return dict(zip(squares, chars))
 
+k = 1
 def assign(values, s, d):
-    return True
+    if k == 1:
+        print "Assign ", s, d 
+    global k 
+    """Eliminate all the other values (except d) from values[s] and propagate.
+    Return values, except return False if a contradiction is detected."""
+    other_values = values[s].replace(d, '')
+    if all(eliminate(values, s, d2) for d2 in other_values):
+        k = 0
+        return values
+    else:
+        k = 0
+        return False
+"""    
+def assign(values, s, d):
+    values[s] = d
+    return values
+"""
+
+def eliminate(values, s, d):
+    """Eliminate d from values[s]; propagate when values or places <= 2.
+    Return values, except return False if a contradiction is detected."""
+    if k == 1:
+        print s, values[s], d 
+    if d not in values[s]:
+        return values ## Already eliminated
+    values[s] = values[s].replace(d,'')
+    ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+    if len(values[s]) == 0:
+        return False ## Contradiction: removed last value
+    elif len(values[s]) == 1:
+        d2 = values[s]
+        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
+            return False
+    ## (2) If a unit u is reduced to only one place for a value d, then put it there.
+    if k == 1:
+        print "....."
+    for u in units[s]:
+        dplaces = [s for s in u if d in values[s]]
+        if len(dplaces) == 0:
+            return False ## Contradiction: no place for this value
+        elif len(dplaces) == 1:
+            print "Dplaces", dplaces
+            # d can only be in one place in unit; assign it there
+            if not assign(values, dplaces[0], d):
+                return False
+    return values
+    
+
+################ Display as 2-D grid ################
+
+def display(values):
+    "Display these values as a 2-D grid."
+    width = 1+max(len(values[s]) for s in squares)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print ''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols)
+        if r in 'CF': print line
+    print
+
 
 g = parse_grid("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
+"""
+  123456789
+A 4.....8.5
+B .3.......
+C ...7.....
+D .2.....6.
+E ....8.4..
+F ....1....
+G ...6.3.7.
+H 5..2.....
+I 1.4......
+"""
 
-print g['A1']
-print g['A2']
+#print g['A1']
+#print g['A2']
+
+print display(g)
