456bf52bf207321f6253d9cbbb7448b984d2df0b
Plotnikov Lev <lev.plotnikov@gmail.com>
2015-08-22 10:51:43 -0700

    move etc.

--- my_stuff/life/life_gui.py
+++ my_stuff/life/life_gui.py
@@ -46,6 +46,23 @@ def ProcessEvent(self, event):
 
 current_speed = 6;
 tick = 0;
+delta_x = 0
+delta_y = 0
+
+move_step = 5
+
+def move_panel(direction):
+    global delta_y, delta_x
+    
+    if direction == K_UP:
+        delta_y -= move_step
+    elif direction == K_DOWN:
+        delta_y += move_step
+    elif direction == K_RIGHT:
+        delta_x += move_step
+    elif direction == K_LEFT:
+        delta_x -= move_step
+
 
 def handle_event(event):
     result = False;
@@ -57,58 +74,43 @@ def handle_event(event):
             # Slow down
             current_speed = current_speed +1;
             tick = 0;
-        elif event.key == K_EQUALS or event.key == K_KP_PLUS:
+        elif event.key == K_EQUALS or event.key == K_KP_PLUS:  # EQUALS is a PLUS
             # Speed up
             current_speed = max(1, current_speed -1);
             tick = 0;
+        elif (event.key == K_DOWN or event.key == K_UP or
+              event.key == K_LEFT or event.key == K_RIGHT):
+            # Move
+            move_panel(event.key);
         else:
-            #print event.type, K_PLUS, K_EQUALS, K_KP_PLUS
-            pass
-        
+            pass      
     else:
         pass
     return result
 
-
-        
-
-def draw_life(w, h, get_next):
-    pygame.init()
-
-    w_clr = (255,255,255) # white color
-    b_clr= (0,0,0)        # black 
-
-    cell_size = 6
-    sq_sz = cell_size
-
-    surface_w = w * cell_size
-    surface_h = h * cell_size + 30 
-    
-    # Create the surface of (width, height), and its window.
-    surface = pygame.display.set_mode((surface_w, surface_h))
-
-    my_clock = pygame.time.Clock()
-
-
+def draw_life(w, h, cell_size, get_next):
     global tick, current_speed
 
+    pygame.init()
+    my_clock = pygame.time.Clock()
+    myfont = pygame.font.SysFont("Courier", 16)
+    w_clr = (255,255,255) # white color
+    b_clr= (0,0,0)        # black
+    
+    surface_w = w * cell_size
+    surface_h = h * cell_size + 30     
+    surface = pygame.display.set_mode((surface_w, surface_h))        # Create the surface of (width, height), and its window.
 
     gen = get_next()
-    tick = current_speed;
-        
-    myfont = pygame.font.SysFont("Courier", 16)
+    tick = current_speed;    
 
-    while True:
-        
-        # Look for an event from keyboard, mouse, etc.
-        # ev = pygame.event.poll() # take only one event
-
+    while True:    
         running = True;
+        
         for event in pygame.event.get():
             if handle_event(event):
-                running = False;
+                running = False;  # Quit
                 break;
-
         if not running:
             break;
 
@@ -122,7 +124,6 @@ def draw_life(w, h, get_next):
         else:
             tick += 1; # just draw the same state
             
-
         my_clock.tick(30)
         surface.fill(w_clr); # fill with white all space
 
@@ -134,8 +135,10 @@ def draw_life(w, h, get_next):
         
         # print "State", st_info
         d = {}
+        # recalculate the position
+        # put in the center 
         for (i,j), _ in d_raw.items():
-            d[(i+(w//2), j+(h//2))] = 1
+            d[(i+(w//2) + delta_x, j+(h//2) + delta_y)] = 1
             
         for (i,j),_ in d.items():
             if i < w and j < h:
@@ -174,7 +177,8 @@ def doit():
         #initial_state = [(-2,-2), (-2,-1), (-2,2), (-1,-2), (-1,1), (0,-2), (0,1), (0,2), (1,0), (2,-2), (2,0), (2,1), (2,2)]
         init0()
         
-    draw_life(160 ,120, gen_life)
+    draw_life(160 ,120, 6, gen_life)
+    #draw_life(1000 ,400, 1, gen_life)
 
 
 
