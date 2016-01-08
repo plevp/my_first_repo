81505d323eef357a0034354b21c0b8196969a43d
Plotnikov Lev <lev.plotnikov@gmail.com>
2016-01-08 21:28:44 +0200

    add echo_client.py and echo_server.py to sockets_c

---
+++ sockets_c/echo_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+import sys
+
+MAXLINE = 4096
+
+def doit(port):
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', port)
+    print 'connecting to %s port %s' % server_address
+    sock.connect(server_address)
+
+    while True:
+        # Send data
+        message = raw_input("message:\n")
+        if message.strip() == "":
+            break
+        print  'sending "%s"' % message
+        sock.sendall(message)
+        
+        # Look for the response
+        data = sock.recv(MAXLINE)
+        print 'received "%s"' % data
+
+    sock.close()
+
+if __name__  == '__main__':
+    doit(int(int(sys.argv[1])))
+
---
+++ sockets_c/echo_server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+import sys
+
+MAXLINE = 4096
+
+def doit(port):
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('localhost', port)
+    print 'starting up on %s port %s' % server_address
+    # Bind the socket to the port
+    sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1)
+
+    while True:
+        print 'waiting for a connection'
+        connection, client_address = sock.accept()
+        print "connection: ", connection, "client_address: ", client_address            
+        print 'connection from', client_address            
+        
+        while True:
+    
+            # Wait for a connection
+            #print 'waiting for a connection'
+            # connection, client_address = sock.accept()
+            #print "connection: ", connection, "client_address: ", client_address                
+            #print 'connection from', client_address
+        
+            # Receive the data in small chunks and retransmit it
+            data = connection.recv(MAXLINE)
+            print  'received "%s"' % data
+            if data:                
+                print 'sending data back to the client'
+                connection.sendall(data)
+            else:
+                print 'no more data from', client_address
+                break
+            
+        connection.close()
+            
+if __name__ == '__main__':
+    doit(int(sys.argv[1]))
